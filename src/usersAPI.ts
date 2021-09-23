b0af431dda3afb67d5e15d448316cd45b00d3d1f
Joe <estephenher999@gmail.com>
2021-09-22 19:11:24 -0500

    add get users by id to users api

--- src/usersAPI.ts
+++ src/usersAPI.ts
@@ -1,36 +1,46 @@
 interface User {
-  id?: number;
-  name: string;
-  age: number;
+    id?: number;
+    name: string;
+    age: number;
 }
 
 let users: Array<User> = [];
 
 export class UsersAPI {
-  createUser(user: Omit<User, "id">) {
-    let id = Math.random() * 1000;
-    let newUser = { id, ...user };
+    createUser(user: Omit<User, 'id'>): User {
+        const id = Math.random() * 1000;
+        const newUser = { id, ...user };
 
-    users.push(newUser);
+        users.push(newUser);
 
-    return newUser;
-  }
-
-  deleteUser(userId: number) {
-    let newUserList = [];
-
-    for (let index = 0; index < users.length; index++) {
-      const user = users[index];
-
-      if (user.id !== userId) {
-        newUserList.push(user);
-      }
+        return newUser;
     }
 
-    users = newUserList;
+    getUserById(userId: number): User {
+        let userFound = null;
 
-    console.log({ users });
+        users.forEach((user: User) => {
+            if (user.id === userId) {
+                userFound = user;
+            }
+        });
 
-    return users;
-  }
+        return userFound;
+    }
+
+    deleteUser(userId: number): User[] {
+        const newUserList = [];
+
+        users.forEach((user: User) => {
+            if (user.id !== userId) {
+                newUserList.push(user);
+            }
+        });
+
+        users = newUserList;
+
+        // console.log({ users });
+
+        return users;
+    }
 }
